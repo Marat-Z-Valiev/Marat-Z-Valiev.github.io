1cff1025c55df16b55b553aaf933e917f46f3e03
MaratGit <mz.valiev@gmail.com>
2017-10-15 22:54:02 -0400

    Portfolio page redesign

--- js/index.js
+++ js/index.js
@@ -1,21 +1,30 @@
 //Select element in the header
-$(".nav a").on("click", function(){
-   $(".nav").find(".active").removeClass("active");
-   $(this).parent().addClass("active");
+$(".nav a").on("click", function() {
+  $(".nav").find(".active").removeClass("active");
+  $(this).parent().addClass("active");
 });
 
 //Scroll to selected section
 $(function() {
   $('.nav-scroll a').click(function(e) {
     e.preventDefault();
-    $('body, html').animate({
-      scrollTop: $($.attr(this, 'href')).offset().top
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top - 50
     }, 750);
   });
 });
 
 //Hide the menu after click
-$('.nav a').on('click', function(){
-    $('.btn-navbar').click(); 
-    $('.navbar-toggle').click();
+$('.nav a').on('click', function() {
+  $('.btn-navbar').click();
+  $('.navbar-toggle').click();
+});
+
+//Scroll to the top
+$("#scrollTop").click(function(e) {
+e.preventDefault();
+$('html, body').animate({
+  scrollTop: 0
+}, 750);
+return false;
 });
