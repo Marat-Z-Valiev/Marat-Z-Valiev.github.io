e26a9706911e0e87d0ab9fb00d8b103ee1e75921
Marat Valiev <mz.valiev@gmail.com>
2018-02-01 22:34:31 -0500

    Updated js file to display current year in the footer

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 //Highlight the element in the header
-$(".nav-item a").on("click", function() {
+$('.nav-item a').on('click', function() {
   $(".nav-item").removeClass("active");
   $(this).parent().addClass("active");
 });
@@ -22,10 +22,14 @@ if($(window).width() < 991){
 }
 
 //Scroll to the top
-$("#scrollTop").click(function(e) {
+$('#scrollTop').click(function(e) {
 e.preventDefault();
 $('html, body').animate({
   scrollTop: 0
 }, 750);
 return false;
 });
+
+
+//Display current year
+$('#currentYear').text((new Date).getFullYear());
